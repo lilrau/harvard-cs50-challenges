c4cbf97e6085ca3752797182f6c531ea41c44049
Raul Souza <raulsscoc@hotmail.com>
2022-04-19 11:10:47 -0300

    Create mario.c

---
+++ mario/mario.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+{
+    int n;
+    do
+        n = get_int("Height: ");
+    while (n<1 || n>8);
+
+    for (int j=n; j>0; j--)
+    {
+        for (int i=j; i>0; i--)
+        {
+            if (i<j)
+                printf(" ");
+            if (i==1)
+                {
+                    for(int k=n; k>=j; k--)
+                        printf("#");
+                    printf("  ");
+                    for(int k=n; k>=j; k--)
+                        printf("#");
+                    printf("\n");
+                }
+        }
+    }
+}
